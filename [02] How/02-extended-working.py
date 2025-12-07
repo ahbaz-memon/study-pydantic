6aaddf8e8c9282f84a006c142cf406f3a43c63d2
Ahbaz <ahbazmemon0@gmail.com>
2025-12-07 21:44:53 +0530

    setting default value

--- [02] How/02-extended-working.py
+++ [02] How/02-extended-working.py
@@ -7,16 +7,16 @@ class Patient(BaseModel):
     name: str
     age: int
     weight: float
-    married: bool
+    married: bool = False # setting default val
     allergies: Optional[List[str]] = None # optional need default val
     contact: Dict[str, str] # dict, where keys & values are str
 
 # raw data
 patient_info = {
     'name': 'Zeeshan',
-    'age': 22,
+    'age': 29,
     'weight': 62,
-    'married': False,
+    # 'married': False,
     # 'allergies': ['Dust', 'Mold', 'Pollen'],
     'contact': {
         'Mobile': '9126673990',
