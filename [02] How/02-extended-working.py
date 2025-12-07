a6421ccd29492654576e4bc88dcb3c9b3feea9ff
Ahbaz <ahbazmemon0@gmail.com>
2025-12-07 21:40:50 +0530

    inserting data according to new schema

--- [02] How/02-extended-working.py
+++ [02] How/02-extended-working.py
@@ -10,3 +10,20 @@ class Patient(BaseModel):
     married: bool
     allergies: List[str] # list of str
     contact: Dict[str, str] # dict, where keys & values are str
+
+# raw data
+patient_info = {
+    'name': 'Zeeshan',
+    'age': 22,
+    'weight': 62,
+    'married': False,
+    'allergies': ['Dust', 'Mold', 'Pollen'],
+    'contact': {
+        'Mobile': '9126673990',
+        'Phone': '020456',
+    },
+}
+
+# calling
+patient = Patient(**patient_info)
+print(patient)
