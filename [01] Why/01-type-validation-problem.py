48225f15deca9c17cb541520e9362da87176717d
Ahbaz <ahbazmemon0@gmail.com>
2025-12-07 13:05:38 +0530

    highlighting problem of type validation

--- [01] Why/01-type-validation-problem.py
+++ [01] Why/01-type-validation-problem.py
@@ -6,3 +6,8 @@ def insert_patient_data(name, age):
     print(age)
 
     print("data inserted!")
+
+# called by junior programmer
+insert_patient_data('Rahul', 'thirty')
+insert_patient_data('Rahul', '30')
+insert_patient_data('Rahul', 30)
