644cda39998ee84f85deb754c67a44d00b175948
Ahbaz <ahbazmemon0@gmail.com>
2025-12-07 14:17:55 +0530

    pydantic object handling scalability and maintainability

--- [02] How/01-simple-working.py
+++ [02] How/01-simple-working.py
@@ -12,6 +12,12 @@ def insert_patient_data(patient: Patient):
 
     print("data inserted!")
 
+def update_patient_data(patient: Patient):
+    print(patient.name)
+    print(patient.age)
+
+    print("data updated!")
+
 # raw data
 # patient_info = {'name': 'Sid', 'age': 'thirty'}
 patient_info = {'name': 'Sid', 'age': '30'}
@@ -19,3 +25,4 @@ patient_info = {'name': 'Sid', 'age': '30'}
 # calling
 patient = Patient(**patient_info)
 insert_patient_data(patient)
+update_patient_data(patient)
