1abf9ec02be993e80bb029f710b4c5b44478da57
Ahbaz <ahbazmemon0@gmail.com>
2025-12-07 22:03:01 +0530

    built-in email validation

---
+++ [02] How/03-general-data-validation.py
@@ -0,0 +1,30 @@
+from pydantic import BaseModel, EmailStr
+from typing import List, Dict, Optional
+
+# ideal schema
+class Patient(BaseModel):
+    
+    name: str
+    age: int
+    email: EmailStr # built-in email validation
+    weight: float
+    married: bool = False # setting default val
+    allergies: Optional[List[str]] = None # optional need default val
+    contact: Dict[str, str] # dict, where keys & values are str
+
+# raw data
+patient_info = {
+    'name': 'Zeeshan',
+    'age': 29,
+    'weight': 62,
+    # 'married': False,
+    # 'allergies': ['Dust', 'Mold', 'Pollen'],
+    'contact': {
+        'Mobile': '9126673990',
+        'Phone': '020456',
+    },
+}
+
+# calling
+patient = Patient(**patient_info)
+print(patient)
