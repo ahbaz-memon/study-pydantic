d6c3651ad43919d5929abaa4c2bc2a66a1393e0b
Ahbaz <ahbazmemon0@gmail.com>
2025-12-07 13:32:14 +0530

    type forcing by python core operators

--- [01] Why/01-type-validation-problem.py
+++ [01] Why/01-type-validation-problem.py
@@ -2,10 +2,13 @@
 def insert_patient_data(name: str, age: int): # type hinting
 
     # assuming database code
-    print(name)
-    print(age)
+    if type(name) == str and type(age) == int: # type forcing
+        print(name)
+        print(age)
 
-    print("data inserted!")
+        print("data inserted!")
+    else:
+        raise TypeError("Wrong data types")
 
 # called by junior programmer
 insert_patient_data('Rahul', 'thirty')
