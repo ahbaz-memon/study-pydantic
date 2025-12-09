909c7a90f8fa1ed7b5919ad44b75f8e22d37b62f
Ahbaz <ahbazmemon0@gmail.com>
2025-12-09 23:48:52 +0530

    specific data validation from field method

---
+++ [02] How/04-specific-data-validation.py
@@ -0,0 +1,33 @@
+from pydantic import BaseModel, EmailStr, AnyUrl, Field
+from typing import List, Dict, Optional
+
+# ideal schema
+class Patient(BaseModel):
+    
+    name: str = Field(max_length=50)
+    age: int = Field(ge=18, lt=60) # 18 <= age < 60
+    email: EmailStr # built-in email validation
+    linkedin: AnyUrl # url validation
+    weight: float = Field(gt=0) # only positive
+    married: bool = False # setting default val
+    allergies: Optional[List[str]] = Field(max_length=5, default=None) # optional need default val
+    contact: Dict[str, str] # dict, where keys & values are str
+
+# raw data
+patient_info = {
+    'name': 'Zeeshan',
+    'age': 29,
+    'email': 'abc@gmail.com',
+    'linkedin': 'https://www.linkedin.com/in/ahbaz-memon/',
+    'weight': 62,
+    'married': False,
+    # 'allergies': ['Dust', 'Mold', 'Pollen'],
+    'contact': {
+        'Mobile': '9126673990',
+        'Phone': '020456',
+    },
+}
+
+# calling
+patient = Patient(**patient_info)
+print(patient)
