ad8186bbc90c360c39d1dedc23c4896033f30f1d
Ahbaz <ahbazmemon0@gmail.com>
2026-01-18 12:32:08 +0530

    simple setup

---
+++ [03] Validator/02-model-validator.py
@@ -0,0 +1,30 @@
+from typing import List, Dict
+from pydantic import BaseModel, EmailStr
+
+class Patient(BaseModel):
+
+    name: str
+    age: int
+    weight: float
+    allergies: List[str]
+    married: bool
+    email: EmailStr
+    contact_details: Dict[str, str]
+
+
+patient_info = {
+    'name': 'Varun',
+    'age': '32',
+    'weight': 77,
+    'allergies': ['Dust', 'Pollen'],
+    'married': True,
+    'email': 'varun88@hdfc.com',
+    'contact_details':  {
+        'Mobile': '8229673990',
+        'Phone': '020976',
+    },
+}
+
+# calling
+patient = Patient(**patient_info)
+print(patient)
