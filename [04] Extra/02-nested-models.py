b6d2d6e23a40950bbd46ef13b7bba8b7b6604afd
Ahbaz <ahbazmemon0@gmail.com>
2026-01-18 17:04:26 +0530

    adding nested model of address

--- [04] Extra/02-nested-models.py
+++ [04] Extra/02-nested-models.py
@@ -2,18 +2,30 @@ from pprint import pprint
 from pydantic import BaseModel
 
 
+class Address(BaseModel):
+    city: str
+    state: str
+    country: str
+    pin_code: int
 
 class Employee(BaseModel):
 
     name: str
     age: int
     gender: str
+    address: Address
 
 
 employee_info = {
     'name': 'Pratik',
     'age': 27,
     'gender': 'male',
+    'address': Address(**{
+        'city': 'Pune',
+        'state': 'Maharashtra',
+        'country': 'India',
+        'pin_code': 411040,
+    })
 }
 employee = Employee(**employee_info)
 pprint(employee)
