e8e07f7fcbd3b0161f005eeb5f61bb13279ea65a
Ahbaz <ahbazmemon0@gmail.com>
2026-01-18 12:41:20 +0530

    validate multiple fields by model validator

--- [03] Validator/02-model-validator.py
+++ [03] Validator/02-model-validator.py
@@ -1,5 +1,5 @@
 from typing import List, Dict
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, model_validator
 
 class Patient(BaseModel):
 
@@ -10,6 +10,15 @@ class Patient(BaseModel):
     married: bool
     email: EmailStr
     contact_details: Dict[str, str]
+
+    @model_validator(mode='after')
+    def check_elderly_have_emergency_contact(cls, model):
+
+        if model.age > 60:
+            if 'Emergency' not in model.contact_details:
+                raise ValueError("Elderly must have emergency contact number")
+        
+        return model
 
 
 patient_info = {
@@ -22,6 +31,7 @@ patient_info = {
     'contact_details':  {
         'Mobile': '8229673990',
         'Phone': '020976',
+        # 'Emergency': '9898989898',
     },
 }
 
